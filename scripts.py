999f4dbaf2605f45562dcec86f0cbec7a9e4c278
valhallajazzy <valhallajazzy@mail.ru>
2023-12-29 00:46:57 +0600

    Refactor scripts.py

--- scripts.py
+++ scripts.py
@@ -1,37 +1,34 @@
-from datacenter.models import *
+from datacenter.models import Schoolkid, Mark, Chastisement, Lesson, Commendation
 from random import choice
 
-from django.core.exceptions import ObjectDoesNotExist
-from django.core.exceptions import MultipleObjectsReturned
+
+CHOISE_OF_PRAISE = ["Молодец!", "Гораздо лучше, чем я ожидал!", "Сказано здорово – просто и ясно!",
+                            "Уже существенно лучше!", "Очень хороший ответ!", "Так держать!"]
+
+
+def get_schoolkid(schoolkid):
+    try:
+        return Schoolkid.objects.get(full_name__contains=schoolkid)
+    except Schoolkid.DoesNotExist:
+        print("Неправильно введены ФИО ученика")
+    except Schoolkid.MultipleObjectsReturned:
+        print("Учеников в данным ФИО найдено более 1. Уточните данные.")
 
 
 def fix_marks(schoolkid):
-    bad_marks = Mark.objects.filter(schoolkid=schoolkid.id, points__in=[2, 3])
-    for mark in bad_marks:
-        mark.points = 5
-        mark.save()
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
 
 
 def remove_chastisements(schoolkid):
-    chastisements = Chastisement.objects.filter(schoolkid=schoolkid.id)
-    for chastisement in chastisements:
-        chastisement.delete()
+    Chastisement.objects.filter(schoolkid=schoolkid).delete()
 
 
-def create_commendation(schoolkid, subject):
+def create_commendation(schoolkid, subject, choise_of_price=CHOISE_OF_PRAISE):
+    random_lesson = Lesson.objects.filter(subject__title=subject, year_of_study=schoolkid.year_of_study,
+                                    group_letter=schoolkid.group_letter).order_by('?').first()
     try:
-        choice_of_praise = ["Молодец!", "Гораздо лучше, чем я ожидал!", "Сказано здорово – просто и ясно!",
-                            "Уже существенно лучше!", "Очень хороший ответ!", "Так держать!"]
-        schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid)
-        lessons = Lesson.objects.filter(subject__title=subject, year_of_study=schoolkid.year_of_study,
-                                        group_letter=schoolkid.group_letter).order_by('-date')
-        random_lesson = choice(lessons)
         Commendation.objects.create(schoolkid=schoolkid, teacher=random_lesson.teacher,
-                                                   subject=random_lesson.subject, created=random_lesson.date,
-                                                   text=choice(choice_of_praise))
-    except ObjectDoesNotExist:
-        print("Неправильно введены ФИО ученика")
-    except MultipleObjectsReturned:
-        print("Учеников в данным ФИО найдено более 1. Уточните данные.")
+                                               subject=random_lesson.subject, created=random_lesson.date,
+                                               text=choice(choise_of_price))
     except IndexError:
         print("Неправильно введен предмет")
